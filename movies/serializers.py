a6e7ee8ba85c7cf11f3e4cc6d1497da91c5cfd9c
NgangaKamau3 <ngangajohn23405@gmail.com>
2025-09-22 05:54:47 +0300

    feat: create movie serializers for API responses

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,3 +1,12 @@
 from rest_framework import serializers
+from .models import Movie
 
-# Movie serializers will be added here
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class MovieListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['tmdb_id', 'title', 'poster_path', 'vote_average', 'release_date']
