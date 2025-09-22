255431ed8092f2b022c52cc203065307233d65c8
NgangaKamau3 <ngangajohn23405@gmail.com>
2025-09-22 05:43:32 +0300

    feat: create basic Movie model with TMDb integration fields

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    tmdb_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    overview = models.TextField(blank=True)
+    release_date = models.DateField(null=True, blank=True)
+    poster_path = models.CharField(max_length=255, blank=True)
+    backdrop_path = models.CharField(max_length=255, blank=True)
+    vote_average = models.FloatField(default=0.0)
+    vote_count = models.IntegerField(default=0)
+    popularity = models.FloatField(default=0.0)
+    genre_ids = models.JSONField(default=list)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-popularity']
+
+    def __str__(self):
+        return self.title
