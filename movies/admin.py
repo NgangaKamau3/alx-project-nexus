93a41b7e1e61756d8dcca29190c0b42b37bd3e71
NgangaKamau3 <ngangajohn23405@gmail.com>
2025-09-22 05:54:59 +0300

    feat: add movie admin interface with search and filters

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'tmdb_id', 'release_date', 'vote_average', 'popularity']
+    list_filter = ['release_date', 'vote_average']
+    search_fields = ['title', 'tmdb_id']
+    readonly_fields = ['created_at', 'updated_at']
