1842976100e82c3d46f3df29506a800bba2c9f8a
NgangaKamau3 <ngangajohn23405@gmail.com>
2025-09-25 06:40:05 +0300

    feat: create user serializers for registration and profile

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,37 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import UserProfile, FavoriteMovie
+from movies.serializers import MovieListSerializer
 
-# User serializers will be added here
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_confirm']
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError("Passwords don't match")
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.CharField(source='user.email', read_only=True)
+    
+    class Meta:
+        model = UserProfile
+        fields = ['username', 'email', 'preferred_genres', 'created_at']
+
+class FavoriteMovieSerializer(serializers.ModelSerializer):
+    movie = MovieListSerializer(read_only=True)
+    
+    class Meta:
+        model = FavoriteMovie
+        fields = ['movie', 'added_at']
