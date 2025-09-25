e6a8fe571c976f49a4d1fa3c1502a827b35dff9c
NgangaKamau3 <ngangajohn23405@gmail.com>
2025-09-25 06:40:27 +0300

    feat: add user admin interface with profile inline

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile, FavoriteMovie
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [UserProfileInline]
+
+@admin.register(FavoriteMovie)
+class FavoriteMovieAdmin(admin.ModelAdmin):
+    list_display = ['user', 'movie', 'added_at']
+    list_filter = ['added_at']
+    search_fields = ['user__username', 'movie__title']
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
