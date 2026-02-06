2e346804e496458ca24bcfc8c424bee9b44d49f4
NgangaKamau3 <ngangajohn23405@gmail.com>
2026-02-06 23:13:25 +0300

    Remove web-based testing pages

--- modestwear/core/views.py
+++ modestwear/core/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("ModestWear API - Visit /test-api/ to test endpoints")
-
-def api_tester(request):
-    return render(request, 'api_tester.html')
+    return render(request, 'index.html')
